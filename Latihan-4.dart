654cfa5ff9444b891c9630e8750f97208b4f2483
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2023-09-08 14:34:07 +0700

    Add files via upload

---
+++ Latihan-4.dart
@@ -0,0 +1,12 @@
+void main(){
+  print('Hello World');
+  print('Eko Kurniawan Khannedy');
+
+  String name;
+
+  name = 'Eko Kurniawan Khannedy';
+  print(name);
+
+  var nama = 'Radi maulana Tsaqif';
+  print(nama);
+}
