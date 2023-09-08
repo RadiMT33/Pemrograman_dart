7046069cb0c987436d12fe6818e9f90099e54080
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2023-09-08 14:02:43 +0700

    Add files via upload

---
+++ Latihan-1.dart
@@ -0,0 +1,11 @@
+void main(){
+  var name = "Radi Maulana Tsaqif";
+  var kelas = "XI PPLG 1";
+  var hobby = "Football";
+  var NoHp = "083844108250";
+
+  print("Nama : $name");
+  print("Kelas : $kelas");
+  print("Hobby : $hobby");
+  print("No Hp : $NoHp");
+}
